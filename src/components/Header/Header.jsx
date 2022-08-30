f3a5aa5e5213f3ffc0dc28ecff6977359b75f9d2
Emma Kanewoff <emmakanewoff@gmail.com>
2022-08-30 15:07:43 +0200

    Header - first draft

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,74 @@
+import "./headerStyle.css";
+import { BsFillPersonFill, BsFillCartFill, BsSearch } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { useState } from "react";
+
+//npm install react-icons
+
 const Header = () => {
-    return ( <div>
-        <h1>Cool Fashion</h1>
-    </div> );
-}
- 
+    // const [showMenu, setShowMenu] = useState(false);
+
+    //   function showbtn() {
+    //     if (show) {
+    //       setShow(false);
+    //     } else {
+    //       setShow(true);
+    //     }
+    //   }
+
+  return (
+    <div>
+      <div className="header">
+        <div className="topRow">
+          <div className="headerMenySearch">
+            <div className="menu">
+              <a href="#">
+                <GiHamburgerMenu />
+              </a>
+            </div>
+
+            <div className="search">
+              <input type="text" id="search" />
+              <a href="#">
+                <BsSearch className="searchIcon" />
+              </a>
+            </div>
+          </div>
+
+          <div className="headerLogo">
+            <h1>Cool Fashion</h1>
+          </div>
+
+          <div className="headerIcons">
+            <a href="#">
+              <BsFillPersonFill />
+            </a>
+            <a href="#">
+              <BsFillCartFill />
+            </a>
+          </div>
+        </div>
+        
+        <div className="menuList">
+            <div className="menuList1">
+                <ul className="categories">
+                    <li><a href="#"> Home </a></li>
+                    <li><a href="#"> Women </a></li>
+                    <li><a href="#"> Unisex </a></li>
+                </ul>
+            </div>
+
+            <div className="menuList2">
+                <ul className="company">
+                    <li><a href="#"> Contact </a></li>
+                    <li><a href="#"> HBTQ </a></li>
+                </ul>
+            </div>
+        </div>
+
+      </div>
+    </div>
+  );
+};
+
 export default Header;
