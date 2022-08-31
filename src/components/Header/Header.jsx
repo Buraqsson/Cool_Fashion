9c7af0ec132f5a3016adfe5161b3411e29186963
Emma Kanewoff <emmakanewoff@gmail.com>
2022-08-31 14:05:29 +0200

    Header finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,16 +6,6 @@ import { useState } from "react";
 //npm install react-icons
 
 const Header = () => {
-    // const [showMenu, setShowMenu] = useState(false);
-
-    //   function showbtn() {
-    //     if (show) {
-    //       setShow(false);
-    //     } else {
-    //       setShow(true);
-    //     }
-    //   }
-
   return (
     <div>
       <div className="header">
@@ -48,25 +38,155 @@ const Header = () => {
             </a>
           </div>
         </div>
-        
-        <div className="menuList">
-            <div className="menuList1">
-                <ul className="categories">
-                    <li><a href="#"> Home </a></li>
-                    <li><a href="#"> Women </a></li>
-                    <li><a href="#"> Unisex </a></li>
-                </ul>
-            </div>
+        {/*<div className="menuList">
+          <div className="menuList1">
+            <ul className="categories">
+              <li>
+                <a href="#"> Home </a>
+              </li>
+              <li>
+                <a href="#"> Women </a>
+              </li>
+              <li>
+                <a href="#"> Unisex </a>
+              </li>
+            </ul>
+          </div>
 
-            <div className="menuList2">
-                <ul className="company">
-                    <li><a href="#"> Contact </a></li>
-                    <li><a href="#"> HBTQ </a></li>
-                </ul>
-            </div>
+          <div className="menuList2">
+            <ul className="company">
+              <li>
+                <a href="#"> Contact </a>
+              </li>
+              <li>
+                <a href="#"> HBTQ </a>
+              </li>
+            </ul>
+          </div>
         </div>
+        */}
+
+        <nav className="navbar navbar-expand-md">
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Summer 22
+                </a>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Women
+                </a>
+                <ul className="dropdown-menu">
+                 <li><a className="dropdown-item" href="#">News</a></li>
+                 <li><a className="dropdown-item" href="#">Dresses</a></li>
+                 <li><a className="dropdown-item" href="#">Shirts / Tops</a></li>
+                 <li><a className="dropdown-item" href="#">Pants / Shorts</a></li>
+                 <li><a className="dropdown-item" href="#">Sweaters</a></li>
+                 <li><a className="dropdown-item" href="#">Shoes</a></li>
+                </ul>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Men
+                </a>
+                <ul className="dropdown-menu">
+                 <li><a className="dropdown-item" href="#">News</a></li>
+                 <li><a className="dropdown-item" href="#">Dresses</a></li>
+                 <li><a className="dropdown-item" href="#">Shirts / Tops</a></li>
+                 <li><a className="dropdown-item" href="#">Pants / Shorts</a></li>
+                 <li><a className="dropdown-item" href="#">Sweaters</a></li>
+                 <li><a className="dropdown-item" href="#">Shoes</a></li>
+                </ul>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Unisex
+                </a>
+                <ul className="dropdown-menu">
+                 <li><a className="dropdown-item" href="#">News</a></li>
+                 <li><a className="dropdown-item" href="#">Dresses</a></li>
+                 <li><a className="dropdown-item" href="#">Shirts / Tops</a></li>
+                 <li><a className="dropdown-item" href="#">Pants / Shorts</a></li>
+                 <li><a className="dropdown-item" href="#">Sweaters</a></li>
+                 <li><a className="dropdown-item" href="#">Shoes</a></li>
+                </ul>
+              </li>
+
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  Sale
+                </a>
+                <ul className="dropdown-menu">
+                 <li><a className="dropdown-item" href="#">Dresses</a></li>
+                 <li><a className="dropdown-item" href="#">Shirts / Tops</a></li>
+                 <li><a className="dropdown-item" href="#">Pants / Shorts</a></li>
+                 <li><a className="dropdown-item" href="#">Sweaters</a></li>
+                 <li><a className="dropdown-item" href="#">Shoes</a></li>
+                </ul>
+              </li>
+            </ul>
+            
+          </div>
+
+          <div className="collapse navbar-collapse " id="navbarSupportedContent">
+            <ul className="navbar-nav  mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Contact
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  HBTQ
+                </a>
+              </li>
+            </ul>
+          </div>
+
+
+
+
+        </div>
+      </nav>
+
+
 
       </div>
+
+
+      
+
+
     </div>
   );
 };
