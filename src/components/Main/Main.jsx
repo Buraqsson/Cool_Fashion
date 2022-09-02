efea3ea72fa41a6a698f63d04e940a9181d537cd
Lina Jepsen <linajepsen@live.se>
2022-09-02 09:37:27 +0200

    update social meida

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import Hero from '../Hero/Hero';
+import React from "react";
+import Hero from "../Hero/Hero";
 import ProductMenu from "../ProduktSida/ProductMenu";
-import './Main.css';
-const Main = () => {
+import "./Main.css";
+import SocialMediaMenu from "../SocialMediaCard/SocialMediaMenu";
 
+const Main = () => {
   let dataBase = [
     {
       title: "https://via.placeholder.com/240x350",
@@ -14,36 +15,38 @@ const Main = () => {
       title: "https://via.placeholder.com/240x350",
 
       text: "Best Sellers",
-    },{
+    },
+    {
       title: "https://via.placeholder.com/240x350",
 
       text: "Tops",
-    },{
+    },
+    {
       title: "https://via.placeholder.com/240x350",
 
       text: "T-shirts",
     },
-    
+
     {
       title: "https://via.placeholder.com/240x350",
 
       text: "Sweatshirts",
-    }, {
+    },
+    {
       title: "https://via.placeholder.com/240x350",
 
       text: "Skirts",
-    }, 
+    },
     {
       title: "https://via.placeholder.com/240x350",
 
       text: "Jeans & Trousers",
-    }, 
+    },
     {
       title: "https://via.placeholder.com/240x350",
 
       text: "Coats & Jackets",
-
-    }, 
+    },
     {
       title: "https://via.placeholder.com/240x350",
 
@@ -56,13 +59,22 @@ const Main = () => {
     },
   ];
 
+  let socialMediaDataBase = [
+    {
+      title: "https://via.placeholder.com/240x350",
+    },
+    {
+      title: "https://via.placeholder.com/240x350",
+    },
+  ];
+
   return (
     <div>
       <Hero />
       <div>
-       <ProductMenu dataBase={dataBase} />
+        <ProductMenu dataBase={dataBase} />
+        <SocialMediaMenu />
       </div>
-      
     </div>
   );
 };
