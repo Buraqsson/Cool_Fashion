65b088ded8bc180894bbb9b2c1f4e107d38d6650
Mohammad Awad <mohammad.awad0110@gmail.com>
2022-09-05 14:35:36 +0200

    update pages

---
+++ src/Pages/FrontPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Main from '../components/Main/Main'
+const FrontPage = () => {
+  return (
+    <div>
+        <Main/>
+    </div>
+  )
+}
+
+export default FrontPage
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,9 +6,6 @@ import { useState } from "react";
 //npm install react-icons
 
 const Header = () => {
-
-
-  array 
   return (
     <div>
       <div className="header">
