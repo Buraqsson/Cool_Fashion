5340e572b3fb0cb7ac54eea6dc104629e6aa1e8e
Anna Blankell <anna.blankell@outlook.com>
2022-09-05 15:18:49 +0200

    Add link to Prod Card.

--- src/components/ProduktSida/ProductCard.jsx
+++ src/components/ProduktSida/ProductCard.jsx
@@ -1,16 +1,13 @@
-import "./ProductCard.css";
+import './ProductCard.css';
 const ProductCard = ({ title, text }) => {
   return (
-    
-    <div className="div_image">
-        {/* <div className=""> */}
-          <img src={title} />
-          <p>{text}</p> 
-        {/* </div> */}
-
+    <div className='div_image'>
+      <a href='https://www.google.com/'>
+        <img src={title} />
+        <p>{text}</p>
+      </a>
     </div>
   );
 };
 
 export default ProductCard;
-
--- src/components/SocialMediaCard/SocialMediaMenu.jsx
+++ src/components/SocialMediaCard/SocialMediaMenu.jsx
@@ -1,30 +1,54 @@
-import "./SocialMedia.css"
-import SocialMediaCard from "./SocialMediaCard.jsx";
+import './SocialMedia.css';
+import SocialMediaCard from './SocialMediaCard.jsx';
 
 const SocialMediaMenu = () => {
   return (
     <div>
       {/* <h3>Social Media menu</h3> */}
       {/* <SocialMediaCard /> */}
+      <div className='socialParent'>
+        <div className='div1'>
+          <a href='https://www.google.com/'>
+            <img
+              className='imgSocialMedia'
+              src='https://via.placeholder.com/290x450?text=Social'
+              alt=''
+            />
+          </a>
+        </div>
 
-      <div className="socialParent">
-        <div className="div1">
-          <img className="imgSocialMedia" src="https://via.placeholder.com/290x450?text=Social" alt="" />
+        <div className='div2'>
+          <a href='https://www.google.com/'>
+            <img
+              className='imgSocialMedia'
+              src='https://via.placeholder.com/290x450?text=Media'
+              alt=''
+            />
+          </a>
         </div>
-        <div className="div2">
-          <img className="imgSocialMedia" src="https://via.placeholder.com/290x450?text=Media" alt="" />
+
+        <div className='div3'>
+          <a href='https://www.google.com/'>
+            <img
+              className='imgSocialMedia'
+              src='https://via.placeholder.com/290x140?text=Images'
+              alt=''
+            />
+          </a>
         </div>
-        <div className="div3">
-          <img className="imgSocialMedia" src="https://via.placeholder.com/290x140?text=Images" alt="" />
+
+        <div className='div4'>
+          <a href='https://www.google.com/'>
+            <p className='socialText'>Make a statement!</p>
+          </a>
         </div>
-        <div className="div4">
-          <p className="socialText">Make a statement!</p>
-        </div>
-        <div className="div5">
-          <p className="socialText">New Customers Gets 20% Off</p>
+
+        <div className='div5'>
+          <a href='https://www.google.com/'>
+            <p className='socialText'>New Customers Gets 20% Off</p>
+          </a>
         </div>
       </div>
-
     </div>
   );
 };
